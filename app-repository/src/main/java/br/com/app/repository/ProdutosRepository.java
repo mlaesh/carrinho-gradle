2c1f0e48e1277e3c464bb739acfe826efd48e5f3
Lais <laismont@outlook.com>
2021-05-13 15:56:38 -0300

    separacao modulo repository

--- app-repository/src/main/java/br/com/app/repository/ProdutosRepository.java
+++ app-repository/src/main/java/br/com/app/repository/ProdutosRepository.java
@@ -2,8 +2,6 @@ package br.com.app.repository;
 
 import br.com.app.model.Produtos;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProdutosRepository extends JpaRepository<Produtos, Long>{
 }
